f41005b7fed596cab4638072ad1f256fdb445fb0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-04-28 15:55:15 +0800

    get kegg id list data

--- viewer/app.ts
+++ viewer/app.ts
@@ -1,1 +1,7 @@
 /// <reference path="../../build/linq.d.ts"/>
+
+$ts(function () {
+    // run web app from here
+    let kegg = $ts.loadJSON("#kegg");
+    let kegg_viewer = new biocad.KEGG.viewer("#data", kegg);
+});
